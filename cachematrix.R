6088194d44db7632859deb31b3d1bf0cfaeb5dee
IDUnderCover <469792116@qq.com>
2015-04-23 16:44:20 +0800

    rprog-013
    Assignments

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two below fucntions compute and cache the inverse of 
+## a square invertible matrix.
 
-## Write a short comment describing this function
 
+##Returns a list that contains four elemets, and each of 
+##them is a funtion.
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y){
+        inv <<- NULL
+        x   <<- y
+    }
+    get<-function(){
+        x
+    }
+    setInv <- function(inverse){
+        inv <<- inverse
+    }
+    getInv <- function(){
+        inv
+    }
+    list(set    = set,
+         get    = get,
+         setInv = setInv,
+         getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## Compute the inverse of 'x'.
+## Note: if  the inverse of 'x' has already been computed and cached,
+##       it will return the inversion matrix without any calculation.
+##       Otherwise, this function will compute and cache the inversion. 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$getInv()
+    if(!is.null(m)){
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    
+    ## make sure that 'x' is invertible
+    dataDim=attributes(data)$dim
+    if(dataDim[1] != dataDim[2]){
+        message("Error: x must be a square matrix")
+    }
+    else{
+        if(det(data) == 0){
+            message("Error: x is an exactly singular matrix")
+        }
+        else{        
+            m <- solve(data, ...)
+            x$setInv(m)
+        }
+    }
+    m
 }
